e9d9d1783d98be7ac80e356bf02ac9a69c819e5d
Carlos Leal <44062740+carloslealp@users.noreply.github.com>
2020-04-01 09:36:34 -0600

    Actualizacion de Scripts
    Se actualizaron los scripts para que empiecen a funcionar los combobox correctamente

--- Scripts/Create proyectoE.sql
+++ Scripts/Create proyectoE.sql
@@ -12,46 +12,67 @@ Use proyectoE
 Go
 
 Create Table Rol(
-	idRol smallint NOT NULL,
-	rol varchar(20) NOT NULL,
-	descripcion varchar(max) NOT NULL
+	idRol smallint NOT NULL
+	,rol varchar(20) NOT NULL
+	,descripcion varchar(max) NOT NULL
 
 	Primary Key (idRol)
 );
 
 Create Table Usuario(
-	dpi bigint NOT NULL,
-	nombre varchar(30) NOT NULL,
-	correo varchar(30) NOT NULL,
-	telefono varchar(30) NOT NULL,
-	idRol smallint NOT NULL,
-	contrasena varchar(20) NOT NULL
+	dpi bigint NOT NULL
+	,nombre varchar(30) NOT NULL
+	,correo varchar(30) NOT NULL
+	,telefono varchar(30) NOT NULL
+	,idRol smallint NOT NULL
+	,contrasena varchar(20) NOT NULL
 
-	Constraint pk_dpiU Primary Key(dpi),
-	Constraint fk_idRolU Foreign Key(idRol) References Rol(idRol)
+	Constraint pk_dpiU Primary Key(dpi)
+	,Constraint fk_idRolU Foreign Key(idRol) References Rol(idRol)
 );
 
 Create Table Departamento(
-	idDepartamento integer identity(1,1) NOT NULL,
-	nombre varchar(50) NOT NULL
+	idDepartamento integer identity(1,1) NOT NULL
+	,nombre varchar(50) NOT NULL
 
 	Constraint pk_idDepartamentoD Primary Key(idDepartamento)
 );
 
 Create Table Municipio(
-	idMunicipio integer identity(1,1) NOT NULL,
-	idDepartamento integer NOT NULL,
-	nombre varchar(30) NOT NULL
+	idMunicipio integer identity(1,1) NOT NULL
+	,idDepartamento integer NOT NULL
+	,nombre varchar(30) NOT NULL
 
-	Constraint pk_idMunicipioM Primary Key(idMunicipio),
-	Constraint fk_idDepartamentoM Foreign Key(idDepartamento) References Departamento(idDepartamento)
+	Constraint pk_idMunicipioM Primary Key(idMunicipio)
+	,Constraint fk_idDepartamentoM Foreign Key(idDepartamento) References Departamento(idDepartamento)
+);
+
+Create Table sectorEmpresa(
+	idSector integer NOT NULL
+	,nombre varchar(20) NOT NULL
+
+	Constraint pk_sector Primary Key (idSector)
 );
 
 Create Table Empresa(
-	nit varchar(20) NOT NULL,
-	contacto varchar(20) NOT NULL,
-	representante varchar(20),
-	direccion varchar(100) NOT NULL,
-	
+	nit varchar(20) NOT NULL
+	,nombre varchar(50) NOT NULL
+	,contacto varchar(20) NOT NULL
+	,representante varchar(20) NOT NULL
+	,direccion varchar(100) NOT NULL
+	,idDepartamento integer NOT NULL
+	--,idMunicipio integer NOT NULL
+	,aldea varchar(50) NOT NULL
+	,telefono1 varchar(10)
+	,telefono2 varchar(10)
+	,correo varchar(20)
+	,paginaWeb varchar(20)
+	,fechaInicio date
+	,descripcionProducto varchar(100)
+	--,idSector integer NOT NULL
+
 	Constraint pk_nit Primary Key(nit)
+	,Constraint fk_idDepartamentoE Foreign Key (idDepartamento) References Departamento(idDepartamento)
+	--,Constraint fk_idMunicipioE Foreign Key (idMunicipio) References Municipio(idMunicipio)
+	--,Constraint fk_idSector Foreign Key (idSector) References sectorEmpresa(idSector)
 );
--- Scripts/Store Procedure proyectoE.sql
+++ Scripts/Store Procedure proyectoE.sql
@@ -2,7 +2,7 @@ Use proyectoE
 Go
 
 Create Procedure sp_login
-	@correo varchar(30)
+	@correo varchar(30)	
 	,@contra varchar(30)
 As
 Select *
@@ -15,11 +15,30 @@ Go
 
 Create Procedure sp_insertSeccion1
 	@nit varchar(20)
+	,@nombre varchar(50)
 	,@contacto varchar(20)
 	,@representante varchar(20)
 	,@direccion varchar(100)
+	,@departamento varchar(50)
+	--,@municipio varchar(30)
+	,@aldea varchar(50)
+	,@telefono1 varchar(10)
+	,@telefono2 varchar(10)
+	,@correo varchar(20)
+	,@paginaWeb varchar(20)
+	,@fechaInicio date
+	,@descripcionProducto varchar(100)
+	--,@sector varchar(20)
 As
-Insert Into Empresa(nit, contacto, representante, direccion)
-Values(@nit, @contacto, @representante, @direccion)
-Go
+Declare @departamentoID integer
+Set @departamentoID = (Select idDepartamento From Departamento Where nombre = @departamento)
+/*Declare @municipioID integer
+Set @municipioID = (Select idMunicipio From Municipio Where nombre = @municipio)
+Declare @sectorID integer
+Set @sectorID = (Select idSector From sectorEmpresa Where nombre = @sector)*/
 
+Insert Into Empresa(nit, nombre, contacto, representante, direccion, idDepartamento, /*idMunicipio,*/
+					aldea, telefono1, telefono2, correo, paginaWeb, fechaInicio, descripcionProducto/*, idSector*/)
+Values(@nit, @nombre, @contacto, @representante, @direccion, @departamentoID, /*@municipioID,*/
+		@aldea, @telefono1, @telefono2, @correo, @paginaWeb, @fechaInicio, @descripcionProducto/*, @sectorID*/)
+Go
